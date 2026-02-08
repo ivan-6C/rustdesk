e34c2fe59162715d3bffcd42a6f3ee0712978e86
ivan-6C <ivan.zheng@sixcolor.com.cn>
2026-02-08 15:18:42 +0800

    common.rs

--- src/common.rs
+++ src/common.rs
@@ -1085,7 +1085,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://rd2.sixcolor.com.cn:21114".to_owned()
 }
 
 #[inline]
